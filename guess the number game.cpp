3cbb9da32385127ed600bd6527676fbc56706c97
VlaDay <98943140+vladay23@users.noreply.github.com>
2024-07-01 20:02:14 +0300

    Update guess the number game.cpp

--- guess the number game.cpp
+++ guess the number game.cpp
@@ -5,38 +5,39 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int hidden, guess, i=0, end=1, games;
     vector<pair<int, int>> res;
     pair<int, int>input;
     ofstream out("result.txt");
-    cout<<" Игра Угадай число"<<endl;
-    cout<<" Укажите количество игр: "<<endl; cin>>games;
-    cout<<" Загадано число от 1 до 100."<<endl;
+    cout<<" Guess the Number game"<<endl;
+    cout<<" Specify the number of games: "<<endl; cin>>games;
+    cout<<" The number from 1 to 100 is hidden."<<endl;
     if (out.is_open()){
         for (int j=1; j<=games; j++){
             i=0;
             end=1;
             srand(time(NULL));
             hidden=rand()%100;
-            cout<<" Номер игры: "<<j<<endl;
-            cout<<" Введите загаданное число: ";
+            cout<<" The sequence number of the game: "<<j<<endl;
+            cout<<" Enter the desired number: ";
             while(end){
                 i++;
                 cin>>guess;
                 input={i, guess};
                 res.push_back(input);
                 if(guess==hidden){
-                    cout<<" Вы выиграли!"<<endl;
-                    cout<<" Вы отгадали число с "<<i<<"-ой попытки!"<<endl;
+                    cout<<" You've won!"<<endl;
+                    cout<<" You guessed the number with "<<i<<" attempts!"<<endl;
                     end=0;
                 }
-                else if(guess>hidden) cout<<" Нет, введённое Вами число, больше задуманного."<<endl;
-                else cout<<" Нет, введённое Вами число, меньше задуманного."<<endl;
+                else if(guess>hidden) cout<<" No, the number you entered is bigger than what you intended.."<<endl;
+                else cout<<" No, the number you entered is less than what you intended."<<endl;
             }
         for (const auto& input :res)
-            out<<" Номер попытки: "<<input.first<<" Введенное число: "<<input.second<<endl;
-        out<<" Загаданное число: "<<hidden<<endl;
+            out<<" Attempt number: "<<input.first<<". The entered number: "<<input.second<<endl;
+        out<<" The hidden number: "<<hidden<<endl;
        }   
     }
     out.close();
